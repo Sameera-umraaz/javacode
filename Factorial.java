920ce7d83adc614ca73e42fddfe0049cd079cb46
sameera <114278490+Sameera-umraaz@users.noreply.github.com>
2022-09-25 21:59:35 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+package basiclogical;
+import java.util.Scanner;
+
+public class Factorial 
+{
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number");
+		int n=s.nextInt();
+		s.close();
+		
+		int fact=1;
+		
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("the factorial of the number is-->"+ fact);
+	}
+
+}
